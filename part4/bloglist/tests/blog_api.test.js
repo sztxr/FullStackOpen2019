e70377cf7e8ac7248dd5dac982923d0bdd411f2c
sztxr <sztxr@github.com>
2019-11-12 14:20:31 -0300

    part 4.9, step 2 (tests)

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -2,12 +2,47 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+  },
+  {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+  },
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const blogObjects = initialBlogs.map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(note => note.save())
+  await Promise.all(promiseArray)
+})
 
 test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body.length).toBe(initialBlogs.length)
+})
+
+test('the unique identifier property is named id instead of _id', async () => {
+  const response = await api.get('/api/blogs')
+  const contents = response.body
+  contents.forEach(blog => expect(blog.id).toBeDefined())
 })
 
 afterAll(() => {
