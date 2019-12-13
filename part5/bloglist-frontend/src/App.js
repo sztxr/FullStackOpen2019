0456df0ef861c188c28329dde82c01df3c6a25a8
sztxr <sztxr@github.com>
2019-12-13 00:52:04 -0300

    part5.1, step1 | add login functionality

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,9 +1,82 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import loginService from './services/login'
+import blogService from './services/blogs'
+import LoginForm from './components/LoginForm'
+import Blog from './components/Blog'
 
 function App() {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  // get all blogs
+  useEffect(() => {
+    const getBlogs = async () => {
+      const blogs = await blogService.getAll();
+      setBlogs(blogs)
+    }
+    getBlogs();
+  }, [])
+
+  // get user details from local storage
+  // useEffect(() => {
+  //   const loggedUserJSON = window.localStorage.getItem('loggedNoteAppUser')
+  //   if (loggedUserJSON) {
+  //     const user = JSON.parse(loggedUserJSON)
+  //     setUser(user)
+  //     blogService.setToken(user.token)
+  //   }
+  // }, [])
+
+  const handleLogin = async e => {
+    e.preventDefault()
+    // console.log('logging in with', username, password)
+
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    }
+    catch (exception) {
+      console.log('Invalid credentials')
+    }
+  }
+
+  const renderItems = () => blogs.map((blog, i) =>
+    <Blog
+      key={i}
+      blog={blog}
+    />
+  )
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <LoginForm
+          handleLogin={handleLogin}
+          username={username}
+          password={password}
+          setUsername={({ target }) => setUsername(target.value)}
+          setPassword={({ target }) => setPassword(target.value)}
+        />
+      </div>
+    )
+  }
+
   return (
     <div>
-      hello
+      <h1>Blogs</h1>
+
+      <p>{user.name} logged in</p>
+      <ul>
+        {renderItems()}
+      </ul>
     </div>
   );
 }
--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,9 @@
 import React from 'react'
+
 const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
+  <li>
+    {blog.title}. {blog.author}
+  </li>
 )
 
 export default Blog
