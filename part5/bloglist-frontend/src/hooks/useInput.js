28954089fc9b6e1a87d18d1636369cb961e61d51
sztxr <sztxr@github.com>
2019-12-14 17:43:45 -0300

    part5-1 | delete unnecessary function

--- part5/bloglist-frontend/src/hooks/useInput.js
+++ part5/bloglist-frontend/src/hooks/useInput.js
@@ -5,14 +5,3 @@ export const useInput = ({ type }) => {
   const input = <input type={type} value={value} onChange={({ target }) => setValue(target.value)} />
   return [value, input]
 }
-
-export const useInputChange = () => {
-  const [input, setInput] = useState({})
-
-  const handleInputChange = (e) => setInput({
-    ...input,
-    [e.currentTarget.name]: e.currentTarget.value
-  })
-
-  return [input, handleInputChange]
-}
