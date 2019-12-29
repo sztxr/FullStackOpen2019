fc4595310149f4b4dc9f8f6773164be647b5833d
sztxr <sztxr@github.com>
2019-12-29 15:58:37 -0300

    part5.17*, step5 | Integration test for auth user

--- part5/bloglist-frontend/src/tests/App.test.js
+++ part5/bloglist-frontend/src/tests/App.test.js
@@ -9,9 +9,29 @@ describe('<App />', () => {
 
     await waitForElement(
       () => component.container.querySelector('.loginForm')
-    ) 
-    
+    )
+
     const heading = component.container.querySelector('h2')
     expect(heading).toHaveTextContent('Log in to application')
+  })
+
+  test('if use is logged, blogs are rendered', async () => {
+    const user = {
+      username: 'tester',
+      token: '01123581321345589',
+      name: 'App Tester'
+    }
+
+    localStorage.setItem('loggedBlogAppUser', JSON.stringify(user))
+
+    const component = render(<App />)
+    component.rerender(<App />)
+
+    await waitForElement(
+      () => component.getByText('Blogs')
+    )
+
+    const blogs = component.container.querySelectorAll('.blogInfo')
+    expect(blogs.length).toBe(2)
   })
 })
